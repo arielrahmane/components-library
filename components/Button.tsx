9da27082d1b5ca6e757eb748c642f7853f4f4327
Ariel Rahmane <ariel@nomoss.co>
2022-03-26 19:41:47 +1100

    navbar component

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,5 +8,5 @@ interface Props {
 export function Button(props: Props): JSX.Element {
   const { children } = props;
 
-  return <button className="p-4 m-4 bg-blue-600">{children}</button>;
+  return <button className="p-4 m-4 text-cyan-500 text-lg font-bold bg-gray-800 shadow-lg rounded-lg shadow-cyan-500/50 w-40 transition duration-300 ease-out hover:scale-105">{children}</button>;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,31 @@
 import type { NextPage } from 'next';
 import { Button } from 'components/Button'
+import { Navbar, NavbarItem, NavbarProps } from 'components/Navbar';
+
+const navbarItems = [
+  {
+    name: "Pictures",
+    href: "pictures"
+  },
+  {
+    name: "Item2"
+  },
+] as NavbarItem[];
+
+const navbarProps = {
+  items: navbarItems,
+  heading: "Navbar Test",
+  backgroundColor: "bg-amber-500"
+} as NavbarProps;
 
 const Home: NextPage = () => {
   return (
-    <Button>
-      Hello
-    </Button>
+    <>
+      <Navbar items={navbarProps.items} heading={navbarProps.heading} backgroundColor={navbarProps.backgroundColor} />
+      <Button>
+        Hello
+      </Button>
+    </>
   );
 };
 
